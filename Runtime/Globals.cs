6734cee0280ec0c67cddacb005860f06e2344791
JFomit <tzaberejny@gmail.com>
2025-05-16 10:17:14 +0300

    Made a brainfuck interpreter in DragoonScript. Now its officially turing-complete.

--- Runtime/Globals.cs
+++ Runtime/Globals.cs
@@ -9,25 +9,50 @@ static class Globals
         ["~-"] = Closure.FromDelegate((int x) => -x),
         ["~+"] = Closure.FromDelegate((int x) => +x),
 
-        ["+"] = Closure.FromDelegate((int a, int b) => a + b),
-        ["-"] = Closure.FromDelegate((int a, int b) => a - b),
-        ["*"] = Closure.FromDelegate((int a, int b) => a * b),
-        ["/"] = Closure.FromDelegate((int a, int b) => a / b),
+        ["+"] = Closure.Overloaded(2,
+            Closure.FromDelegate((int a, int b) => a + b),
+            Closure.FromDelegate((byte a, byte b) => (byte)(a + b))),
+        ["-"] = Closure.Overloaded(2,
+            Closure.FromDelegate((int a, int b) => a - b),
+            Closure.FromDelegate((byte a, byte b) => (byte)(a - b))),
+        ["*"] = Closure.Overloaded(2,
+            Closure.FromDelegate((int a, int b) => a * b),
+            Closure.FromDelegate((byte a, byte b) => (byte)(a * b))),
+        ["/"] = Closure.Overloaded(2,
+            Closure.FromDelegate((int a, int b) => a / b),
+            Closure.FromDelegate((byte a, byte b) => (byte)(a / b))),
+        ["%"] = Closure.Overloaded(2,
+            Closure.FromDelegate((int a, int b) => a % b),
+            Closure.FromDelegate((byte a, byte b) => (byte)(a % b))),
 
-        [">"] = Closure.FromDelegate((int a, int b) => a > b),
-        ["<"] = Closure.FromDelegate((int a, int b) => a < b),
-        [">="] = Closure.FromDelegate((int a, int b) => a >= b),
-        ["<="] = Closure.FromDelegate((int a, int b) => a <= b),
-        ["=="] = Closure.Overloaded(2, Closure.FromDelegate((int a, int b) => a == b), Closure.FromDelegate((char a, char b) => a == b)),
-        ["!="] = Closure.Overloaded(2, Closure.FromDelegate((int a, int b) => a != b), Closure.FromDelegate((char a, char b) => a != b)),
+        [">"] = Closure.Overloaded(2,
+            Closure.FromDelegate((int a, int b) => a > b),
+            Closure.FromDelegate((byte a, byte b) => a > b)),
+        ["<"] = Closure.Overloaded(2,
+            Closure.FromDelegate((int a, int b) => a < b),
+            Closure.FromDelegate((byte a, byte b) => a < b)),
+        [">="] = Closure.Overloaded(2,
+            Closure.FromDelegate((int a, int b) => a >= b),
+            Closure.FromDelegate((byte a, byte b) => a >= b)),
+        ["<="] = Closure.Overloaded(2,
+            Closure.FromDelegate((int a, int b) => a <= b),
+            Closure.FromDelegate((byte a, byte b) => a <= b)),
+        ["=="] = Closure.Overloaded(2,
+            Closure.FromDelegate((int a, int b) => a == b),
+            Closure.FromDelegate((char a, char b) => a == b),
+            Closure.FromDelegate((byte a, byte b) => a == b)),
+        ["!="] = Closure.Overloaded(2,
+            Closure.FromDelegate((int a, int b) => a != b),
+            Closure.FromDelegate((char a, char b) => a != b),
+            Closure.FromDelegate((byte a, byte b) => a != b)),
 
         ["++"] = Closure.FromDelegate((object x, object y) => $"{x}{y}"),
 
-        ["print"] = Closure.FromDelegate((object x) =>
+        ["printline"] = Closure.FromDelegate((object x) =>
         {
             if (x is Unit u)
             {
-                Console.WriteLine("()");
+                Console.WriteLine();
             }
             else if (x is IClosure cl)
             {
@@ -39,9 +64,30 @@ static class Globals
             }
             return Prelude.Unit;
         }),
+        ["print"] = Closure.FromDelegate((object x) =>
+        {
+            if (x is Unit u)
+            {
+                Console.Write("()");
+            }
+            else if (x is IClosure cl)
+            {
+                Console.Write(cl.Format());
+            }
+            else
+            {
+                Console.Write(x);
+            }
+            return Prelude.Unit;
+        }),
         ["read"] = Closure.FromDelegate((object x) =>
         {
             var str = Console.ReadLine()!;
+            return str;
+        }),
+        ["read_char"] = Closure.FromDelegate((object x) =>
+        {
+            var str = (byte)Console.ReadKey().KeyChar;
             return str;
         }),
         ["random"] = Closure.FromDelegate((int a, int b) =>
@@ -93,6 +139,57 @@ static class Globals
                 return Prelude.Unit;
             }
             throw new InterpreterException($"Invalid type: expected array, got {array.GetType().Format()}.", Prelude.None);
-        }).Curry()
+        }).Curry(),
+        ["length_of"] = Closure.FromDelegate((object array) =>
+        {
+            if (array is Array arr)
+            {
+                return arr.Length;
+            }
+            else if (array is string str)
+            {
+                return str.Length;
+            }
+            throw new InterpreterException($"Invalid type: expected string or array, got {array.GetType().Format()}.", Prelude.None);
+        }),
+        ["to_byte"] = Closure.FromDelegate((object value) =>
+        {
+            if (value is int i)
+            {
+                return (byte)i;
+            }
+            else if (value is byte b)
+            {
+                return b;
+            }
+            else if (value is char c)
+            {
+                return (byte)c;
+            }
+            else
+            {
+                return value.ValueCast<byte>();
+            }
+        }),
+        ["to_char"] = Closure.FromDelegate((object value) =>
+        {
+            if (value is int i)
+            {
+                return (char)i;
+            }
+            else if (value is byte b)
+            {
+                return (char)b;
+            }
+            else if (value is char c)
+            {
+                return c;
+            }
+            else
+            {
+                return value.ValueCast<char>();
+            }
+        }),
+        ["failwith"] = Closure.FromDelegate<string, Absurd>((string msg) => throw new InterpreterException(msg, Prelude.None))
     });
 }
